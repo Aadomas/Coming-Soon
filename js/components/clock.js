bd09a35fb56872ee82152eeaf0fd2443c9a840d0
Adomas <okamjoreikia@gmail.com>
2021-01-28 16:26:37 +0200

    tik tok

--- js/components/clock.js
+++ js/components/clock.js
@@ -1,9 +1,10 @@
 function clock(selector) {
-    const DOM = document.querySelector(selector)
-    const numbers = [432, 9, 37, 39];
-    const labels = ["days", "hours", "minutes", "seconds"];
-    let HTML = "";
-
+    const clockDOM = document.querySelector(selector);
+    let allValuesDOM = null;
+    const deadline = '01-04 14:00:00';
+    let numbers = calcTime(deadline);
+    const labels = ['days', 'hours', 'minutes', 'seconds'];
+    let HTML = '';
 
     for (let i = 0; i < 4; i++) {
         HTML += `<div class="time">
@@ -11,14 +12,51 @@ function clock(selector) {
                     <div class="label">${labels[i]}</div>
                 </div>`;
     }
-    DOM.innerHTML = HTML;
+
+    clockDOM.innerHTML = HTML;
+    allValuesDOM = document.querySelectorAll(`${selector} .value`);
+
+    setInterval(function () {
+        numbers = calcTime(deadline);
+        for (let i = 0; i < 4; i++) {
+            allValuesDOM[i].innerText = numberFormat(numbers[i]);
+        }
+    }, 1000);
 }
 
 function numberFormat(number) {
     if (number < 10) {
-        return "0" + number;
+        return '0' + number;
     }
     return number;
 }
 
-export {clock}
+function calcTime(deadline) {
+    const date = new Date();
+    const now = Date.now();
+    let year = date.getFullYear();
+    let fullDeadline = `${year}-${deadline}`;
+    let fullDeadlineInMiliseconds = (new Date(fullDeadline)).getTime();
+
+    if (fullDeadlineInMiliseconds < now) {
+        year++;
+        fullDeadline = `${year}-${deadline}`;
+        fullDeadlineInMiliseconds = (new Date(fullDeadline)).getTime();
+    }
+
+    const diff = fullDeadlineInMiliseconds - now;
+    const seconds = Math.round(diff / 1000);
+
+    const days = Math.floor(seconds / 60 / 60 / 24);
+    let unusedSeconds = seconds - days * 60 * 60 * 24;
+
+    const hours = Math.floor(unusedSeconds / 60 / 60);
+    unusedSeconds -= hours * 60 * 60;
+
+    const minutes = Math.floor(unusedSeconds / 60);
+    unusedSeconds -= minutes * 60;
+
+    return [days, hours, minutes, unusedSeconds];
+}
+
+export { clock }
