64a7cf9f81ed6d6c3fa977ee31173d20bdad699a
Adomas <okamjoreikia@gmail.com>
2021-02-03 16:06:09 +0200

    update index + prgrs bar

---
+++ js/data/progressBarData.js
@@ -0,0 +1,16 @@
+const progressBarData = [
+    {
+        label:"UX Design",
+        value:90
+    },
+    {
+        label:"Web Design",
+        value:85
+    },
+    {
+        label:"Web Development",
+        value:85
+    },
+]
+
+export { progressBarData }
