c05851ae6d09d213a5f2bf2dca6b3bfd4b047cdf
Adomas <okamjoreikia@gmail.com>
2021-02-04 16:44:05 +0200

    progressbar

--- js/data/progressBarData.js
+++ js/data/progressBarData.js
@@ -1,16 +1,64 @@
 const progressBarData = [
+    141526,
+    'asfd',
+    true,
+    false,
+    null,
+    undefined,
+    [],
+    {},
     {
-        label:"UX Design",
-        value:90
+        label: null
     },
     {
-        label:"Web Design",
-        value:86
+        label: ''
     },
     {
-        label:"Web Development",
-        value:50
+        label: 1452
     },
-]
+    {
+        label: 'Programming'
+    },
+    {
+        label: 'Programming',
+        value: null
+    },
+    {
+        label: 'Programming',
+        value: 'random'
+    },
+    {
+        label: 'Programming',
+        value: Infinity
+    },
+    {
+        label: 'Programming',
+        value: NaN
+    },
+    {
+        label: 'Programming',
+        value: 120
+    },
+    {
+        label: 'Programming',
+        value: -120
+    },
+    {
+        label: ' ',
+        value: 30
+    },
+    {
+        label: 'UX Design',
+        value: 90
+    },
+    {
+        label: 'Web design',
+        value: 30
+    },
+    {
+        label: 'Web development',
+        value: 50
+    },
+];
 
 export { progressBarData }
